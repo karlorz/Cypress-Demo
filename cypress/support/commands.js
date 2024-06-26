6db378f5d8821f6d9466ac15187d5399fce608d5
Karl Chow <142769090+karlorz@users.noreply.github.com>
2024-06-26 20:58:23 +0800

    123

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (username, password) => {
+  // Caching session when logging in via page visit
+  cy.session(name, () => {
+    cy.visit("/login");
+    cy.get("[data-test=name]").type(name);
+    cy.get("[data-test=password]").type("s3cr3t");
+    cy.get("form").contains("Log In").click();
+    cy.url().should("contain", "/login-successful");
+  });
+
+  // Caching session when logging in via API
+  cy.session(username, () => {
+    cy.request({
+      method: "POST",
+      url: "/login",
+      body: { username, password },
+    }).then(({ body }) => {
+      window.localStorage.setItem("authToken", body.token);
+    });
+  });
+});
